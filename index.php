2b3eba8fbaed4af3ca0a86b79f3022159aff73d5
Kristine Costo <starving_artist@Kristines-MacBook-Pro.local>
2020-10-28 12:57:07 +0800

    uploading files for Web Developer Practical Exam of Valens Research

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Juan's Auto Paint | Landing</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<!-- new addition -->
+
+	<meta data-n-head="true" charset="utf-8" />
+	
+	<meta data-hid="description" name="description" content="Cool School" />
+
+	<meta name="theme-color" content="#000000" />
+
+	<meta name="google" value="notranslate" />
+
+	<link rel="icon" type="image/x-icon" href="assets/images/board.png">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/juansautopaint.css">
+
+</head>
+<body>
+
+
+<section class="index-container">
+
+	<header>
+		<h1>
+			JUAN'S AUTO PAINT
+		</h1>
+	</header>
+
+	<!-- /header -->
+
+	<section>
+		
+		<h6>
+			NEW PAINT JOB
+		</h6>
+
+		<h3>
+			New Paint Job
+		</h3>
+
+		<div class="sidebyside">
+
+			<div>
+			
+				<h5>
+					Paint Jobs in Progress
+				</h5>
+
+				<table class="grey">
+
+					<thead>
+						<tr>
+							<th class="grey">Plate No.</th>
+							<th class="grey">Current Color</th>
+							<th class="grey">Target Color</th>
+							<th class="grey">Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="lefttable, grey">XSA 723</td>
+							<td class="lefttable, grey">Blue</td>
+							<td class="lefttable, grey">Green</td>
+							<td class="lefttable, grey" style="color: red;">Mark as Completed</td>
+						</tr>
+						<tr>
+							<td class="lefttable, grey">ABS 092</td>
+							<td class="lefttable, grey">Red</td>
+							<td class="lefttable, grey">Blue</td>
+							<td class="lefttable, grey" style="color: red;">Mark as Completed</td>
+						</tr>
+						<tr>
+							<td class="lefttable, grey">JSL 110</td>
+							<td class="lefttable, grey">Red</td>
+							<td class="lefttable, grey">Green</td>
+							<td class="lefttable, grey" style="color: red;">Mark as Completed</td>
+						</tr>
+						<tr>
+							<td class="lefttable, grey">XHJ 133</td>
+							<td class="lefttable, grey">Green</td>
+							<td class="lefttable, grey">Blue</td>
+							<td class="lefttable, grey" style="color: red;">Mark as Completed</td>
+						</tr>
+						<tr>
+							<td class="lefttable, grey">HSU 005</td>
+							<td class="lefttable, grey">Blue</td>
+							<td class="lefttable, grey">Red</td>
+							<td class="lefttable, grey" style="color: red;">Mark as Completed</td>
+						</tr>
+					</tbody>
+
+				</table>
+			</div>
+
+			<div class="tableright">
+				
+				<h5 class="lower" style="color: white;">
+					SHOP PERFORMANCE
+				</h5>
+
+				<table>
+
+					<thead>
+						<tr>
+							<th class="none">Total Cars Painted:</th>
+							<th class="none" style="font-weight: bold;">48</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="none">Breakdown:</td>
+							<td class="none"></td>
+						</tr>
+						<tr>
+							<td class="none">Blue</td>
+							<td class="none" style="font-weight: bold;">24</td>
+						</tr>
+						<tr>
+							<td class="none">Red</td>
+							<td class="none" style="font-weight: bold;">21</td>
+						</tr>
+						<tr>
+							<td class="none">Green</td>
+							<td class="none" style="font-weight: bold;">3</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+
+		<h5>
+			Car Details
+		</h5>
+
+		<h6>
+			Paint Queue
+		</h6>
+
+		<table class="grey">
+
+			<thead>
+				<tr class="grey">
+					<th>Plate No.</th>
+					<th>Current Color</th>
+					<th>Target Color</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr class="grey">
+					<td>KKL 989</td>
+					<td>Blue</td>
+					<td>Green</td>
+				</tr>
+				<tr>
+					<td>HJR 279</td>
+					<td>Red</td>
+					<td>Blue</td>
+				</tr>
+				<tr>
+					<td>FGL 116</td>
+					<td>Red</td>
+					<td>Green</td>
+				</tr>
+				<tr class=grey-right>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+
+
+	</section>
+
+
+	<!-- JQuery Set-Up for Bootstrap: open getbootstrap.com, Download, BootstrapCDN-->
+	<!-- script for jQuery -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+
+	<!-- script for Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	
+	<!-- script for Bootstrap: we downloaded this code from Bootstrap and saved it in the repository folder (local and git) -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+
+
+
+</body>
+</html>
